8d90453798db64d12a9b489d97f531e97a178df4
e601201 <k.arthur1111@gmail.com>
2022-05-26 19:31:03 +0900

    "[ADD]person_model_as_user"

---
+++ app/controllers/person_sessions_controller.rb
@@ -0,0 +1,20 @@
+class PersonSessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+  def new;end
+
+  def create
+    @person = login(params[:email], params[:password])
+
+    if @person
+      redirect_back_or_to foods_path, notice: 'Login successful'
+    else
+      flash.now[:alert] = 'Login failed'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to foods_path, notice: 'Logged out!'
+  end
+end
