4e9ceeec44d8fb74e40f7edb97f98cb2d224579a
e601201 <k.arthur1111@gmail.com>
2022-05-29 04:35:50 +0900

    "[MOD]a_little_change"

--- app/loyalties/kinds_loyalty.rb
+++ app/loyalties/kinds_loyalty.rb
@@ -1,21 +1,21 @@
 class KindsLoyalty < ApplicationLoyalty
   def update?
-    user.role_admin? || user.role_editor?
+    user&.role_admin? || user&.role_editor?
   end
 
   def new?
-    user.role_admin? || user.role_editor?
+    user&.role_admin? || user&.role_editor?
   end
 
   def create?
-    user.role_admin? || user.role_editor?
+    user&.role_admin? || user&.role_editor?
   end
 
   def update?
-    user.role_admin? || user.role_editor?
+    user&.role_admin? || user&.role_editor?
   end
 
   def destroy?
-    user.role_admin? || user.role_editor?
+    user&.role_admin? || user&.role_editor?
   end
 end
