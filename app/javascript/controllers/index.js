487d1651e13d53b489629ae41c0319b8be253d28
DaichiNagata <90130769+e601201@users.noreply.github.com>
2022-07-05 00:31:07 +0900

    Revert "Instant search mode"

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,12 +2,6 @@
 
 import { application } from "controllers/application"
 
-//jsbundling-railsをしようする場合は、下記のようなFormControllerを登録するコードが追加されているよ。
-//import FormController from "./form_controller.js"
-//application.register("form", FormController)
-
-//今回はimportmap-railsを使うので、Stimulusコントローラーは自動で登録されます。index.jsにはコードは追加されないよ。
-
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
